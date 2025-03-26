2556797944c04ca73066e35f6e57cc36b6557dd8
Gowtham <curiousgowtham@gmail.com>
2025-03-26 21:09:32 +0530

    signup_page

--- bookmydesk/accounts/urls.py
+++ bookmydesk/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import test_view
+from accounts.views import signup_view, home_view
 
 urlpatterns = [
-    path('test/', test_view)
+    path('signup/', signup_view, name='signup'),
+    path('home/', home_view, name="home"),
 ]
--- bookmydesk/accounts/views.py
+++ bookmydesk/accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from django.contrib.auth import login
 
-def test_view(request):
-    return HttpResponse("BookMyDesk")
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignupForm()
+    return render(request, template_name='signup.html', context={'form': form})
+
+def home_view(request):
+    return render(request, "home.html")
