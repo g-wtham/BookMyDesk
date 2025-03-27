8abbb6c9ef374115395468ed70c3c288714222ef
Gowtham <curiousgowtham@gmail.com>
2025-03-27 11:32:35 +0530

    login_page

--- bookmydesk/accounts/urls.py
+++ bookmydesk/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import signup_view, home_view
+from accounts.views import signup_view, login_view, home_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('home/', home_view, name="home"),
+    path('login/', login_view, name="login"),
 ]
--- bookmydesk/accounts/views.py
+++ bookmydesk/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 def signup_view(request):
     if request.method == 'POST':
@@ -15,3 +16,17 @@ def signup_view(request):
 
 def home_view(request):
     return render(request, "home.html")
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, template_name="login.html", context= {'login_form': form})
